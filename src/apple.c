30d5bef4909b9ff103c46bbb293dea5172df9ebe
Ryoga Masaki <cassadeesr75@icloud.com>
2020-07-06 09:25:41 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,31 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int x){
+  int sum = 0;
+  int c;
+  for(c = 0; c < n; c++){
+    sum += (A[c] + x - 1)/x;
+  }
+  return sum <= k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000;
+  while(ub - lb > 1){
+    int mid = (lb + ub)/2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else{
+      lb = mid;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
